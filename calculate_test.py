920c8f0b67f1f6c2c6806d9623a81d2cee05258e
Nicola <nicspanu@gmail.com>
2021-02-18 11:13:19 +0100

    unittest 4

--- calculate_test.py
+++ calculate_test.py
@@ -6,7 +6,12 @@ class TestCalculate(unittest.TestCase):
         self.calc = Calculate()
 
     def test_add_method_returns_correct_result(self):
-        self.assertEqual(self.calc.add("Hello", "World"), "Helloworld!")
+        self.assertEqual(self.calc.add(2, 2), 4)
+
+    def test_add_method_raises_typeerror_if_not_ints(self):
+        self.assertRaises(TypeError, self.calc.add, "Hello", "World")
+
+
 
 if __name__ == '__main__':
     unittest.main()
