607ddf618cf4c41ccc9a2d87d344f61e87046c9e
Nicola <>
2021-02-19 09:32:02 +0100

    mock

---
+++ mock_example_test.py
@@ -0,0 +1,13 @@
+import unittest
+from mock import Mock
+
+
+class TestMocking(unittest.TestCase):
+    def test_mock_method_returns(self):
+        my_mock = Mock()
+        my_mock.my_method.return_value = "hello"
+        self.assertEqual("hello", my_mock.my_method())
+
+
+if __name__ == '__main__':
+unittest.main()
