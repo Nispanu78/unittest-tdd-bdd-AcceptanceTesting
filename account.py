873c7608a0417bc637d83f804c47e0f5b42886f6
Nicola <nicspanu@gmail.com>
2021-02-19 10:07:03 +0100

    mock 5

--- account.py
+++ account.py
@@ -4,3 +4,12 @@ class Account(object):
 
     def get_account(self, id_num):
         return self.di.get(id_num)
+
+class ConnectionError(Exception):
+    pass
+    def get_account(self, id_num):
+        try:
+            result = self.di.get(id_num)
+        except ConnectionError:
+            result = "Connection error occurred. Try Again."
+        return result
--- account_test.py
+++ account_test.py
@@ -1,6 +1,6 @@
 import unittest
 from mock import Mock
-from account import Account
+from account import Account, ConnectionError
 
 
 class TestAccount(unittest.TestCase):
@@ -10,6 +10,12 @@ class TestAccount(unittest.TestCase):
         mock_data_interface.get.return_value = account_data
         account = Account(mock_data_interface)
         self.assertDictEqual(account_data, account.get_account(1))
+
+    def test_account_when_connect_exception_raised(self):
+        mock_data_interface = Mock()
+        mock_data_interface.get.side_effect = ConnectionError()
+        account = Account(mock_data_interface)
+        self.assertEqual("Connection error occurred. Try Again.", account.get_account(1))
 
 
 if __name__ == '__main__':
