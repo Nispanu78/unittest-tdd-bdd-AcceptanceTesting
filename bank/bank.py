d820e6075d4797b4f8c0cfca562d1495d9c8f2bd
Nicola <nicspanu@gmail.com>
2021-02-21 16:16:02 +0100

    tdd 5

--- bank/bank.py
+++ bank/bank.py
@@ -1,3 +1,9 @@
 class Bank(object):
     def __init__(self):
         self.accounts = {}
+
+    def add_account(self, account):
+        self.accounts[account.account_number] = account.balance
+
+    def get_account_balance(self, account_number):
+        return self.accounts.get(account_number)
