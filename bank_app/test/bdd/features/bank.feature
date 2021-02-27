bc05d22028597e85b324e9d59221dd5335b27297
Nicola <nicspanu@gmail.com>
2021-02-27 11:35:34 +0100

    acceptance 6

--- bank_app/test/bdd/features/bank.feature
+++ bank_app/test/bdd/features/bank.feature
@@ -5,7 +5,9 @@ Feature: Bank web application to retrieve
     and withdraw from my balance
 
 Scenario: Retrieve customer balance
-Given I create account "1111" with balance of "50"
-And I visit the homepage
-When I enter the account number "1111"
-Then I see a balance of "50"
+    	Given I create the following account:
+            | account_number | balance |
+    		| 1111           | 50      |
+    	And I visit the homepage
+    	When I enter the account number "1111"
+    	Then I see a balance of "50"
--- bank_app/test/bdd/steps/step.py
+++ bank_app/test/bdd/steps/step.py
@@ -24,7 +24,8 @@ def i_see_a_balance_of_group1(step, expected_balance):
     assert_in ("Balance: {}".format(expected_balance),
               world.form_response.text)
 
-@step(u'I create account "([^"]*)" with balance of "([^"]*)"')
-def i_create_account_with_balance_of_group1(step, account_number, balance):
-    a = Account(account_number, balance)
-    BANK.add_account(a)
+@step(u'I create the following account:')
+def i_create_the_following_account(step):
+    for row in step.hashes:
+        a = Account(row['account_number'], row['balance'])
+        BANK.add_account(a)
