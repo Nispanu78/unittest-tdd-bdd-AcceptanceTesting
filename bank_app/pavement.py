db9c870330661a13ac4a6edd46c7e8b4cec184a3
Nicola <nicspanu@gmail.com>
2021-03-01 10:44:54 +0100

    jenkins 3

--- bank_app/pavement.py
+++ bank_app/pavement.py
@@ -1,6 +1,15 @@
-from paver.tasks import task
+from paver.tasks import task, BuildFailure, needs
 from paver.easy import sh
 
 @task
 def unit_tests():
-    sh('nosetests test/unit')
+    sh('nosetests --with-coverage test/unit')
+
+@task
+def lettuce_tests():
+    sh('lettuce test/bdd')
+
+@needs('unit_tests', 'lettuce_tests', 'run_pylint', 'sdist')
+@task
+def default():
+    pass
